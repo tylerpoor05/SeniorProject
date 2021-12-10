5354525c3eb596f27eeacc63e177f31f9b2248b6
tylerpoor05 <65245471+tylerpoor05@users.noreply.github.com>
2021-12-10 15:49:19 -0500

    Add files via upload

---
+++ ADRemediations.ps1
@@ -0,0 +1,10 @@
+﻿Get-ADUser -Filter * -SearchBase “OU=Users,DC=CSUDC01,DC=csu.local” | Set-ADUser -CannotChangePassword:$false -PasswordNeverExpires:$false -ChangePasswordAtLogon:$true
+Set-ADUser -Identity <samAccountName> -ChangePasswordAtLogon $true
+
+Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Services\LanmanServer\Parameters" SMB1 -Type DWORD -Value 0 –Force
+
+REG ADD  “HKLM\Software\policies\Microsoft\Windows NT\DNSClient”
+REG ADD  “HKLM\Software\policies\Microsoft\Windows NT\DNSClient” /v ” EnableMulticast” /t REG_DWORD /d “0” /f
+
+$regkey = "HKLM:SYSTEM\CurrentControlSet\services\NetBT\Parameters\Interfaces"
+Get-ChildItem $regkey |foreach { Set-ItemProperty -Path "$regkey\$($_.pschildname)" -Name NetbiosOptions -Value 2 -Verbose}
